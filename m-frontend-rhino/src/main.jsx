859aaf3220cb43f4352c08ffde10dd1a92bea21e
MahnoorKhawaja <141573018+MahnoorKhawaja@users.noreply.github.com>
2025-10-19 16:02:28 +0500

    Update main.jsx

--- m-frontend-rhino/src/main.jsx
+++ m-frontend-rhino/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './css/index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './css/index.css';
+import App from './App.jsx';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+const PUBLISHABLE_KEY = 'pk_test_cG9saXNoZWQtcGhvZW5peC01My5jbGVyay5hY2NvdW50cy5kZXYk';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  </StrictMode>
+);
